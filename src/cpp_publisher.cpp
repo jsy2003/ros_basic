84cf5442442cb3b610f08b733418d6118f775b3b
jsy2003 <93853610+jsy2003@users.noreply.github.com>
2021-11-12 20:13:44 +0900

    Update cpp_publisher.cpp

--- src/cpp_publisher.cpp
+++ src/cpp_publisher.cpp
@@ -3,5 +3,29 @@
 
 int main(int argc, char **argv)
 {
+    ros::init(argc, argv, "publisher_cpp");
+    ros::NodeHandle nh;
     
+    /*
+        A Publisher is created in the below line
+        First argument is the name of the topic.
+        Second argument is queue_size which is used to buffer up a maximum of 10 messages 
+            before beginning to throw away old ones if we are publishing too quickly
+        This publisher is using Int32 message to publish data on "topic_cpp" 
+    */
+    ros::Publisher pub = nh.advertise<std_msgs::Int32>("topic_cpp", 10);
+    ROS_INFO("publisher_cpp node started and publishing data on topic_cpp");
+    ros::Rate loop_rate(10);
+    
+    std_msgs::Int32 count;  
+    count.data = 0;
+    
+    while(ros::ok())
+    {
+        pub.publish(count);
+        count.data++;
+        loop_rate.sleep();
+    }
+    
+    return 0;
 }
