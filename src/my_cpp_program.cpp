fc7f74e043e87b3b9db5f912aab8b9ca6736d25f
jsy2003 <93853610+jsy2003@users.noreply.github.com>
2021-12-01 15:02:19 +0900

    Update my_cpp_program.cpp

--- src/my_cpp_program.cpp
+++ src/my_cpp_program.cpp
@@ -1,1 +1,24 @@
 #include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+
+void chatterCallback(const sensor_msgs::LaserScan::ConstPtr &scan)
+{
+    int size = scan->range.size();
+    int middle = size/2;
+    float range = scan->range[middle];
+  
+    ROS_INFO("Range[%d] = %f", middle, range);
+  
+}
+
+int main(int argc, char *argv)
+{
+    ros::init(argc, argv, "my_node");
+    ros::NodeHandle nh;
+  
+    ros::Subscriber sub = nh.subscribe("/sick_s300_front/scan". 1, chatterCallback);
+  
+    ros::spin();
+  
+    return 0;
+}
